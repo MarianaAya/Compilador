1c22520f60702af02a501378ee538670a391d8e6
Mariana Aya <mariana.aya@eurekalabs.com.br>
2022-04-21 19:22:19 -0300

    .

--- src/compilador/models/GeracaoCodigoObjeto.java
+++ src/compilador/models/GeracaoCodigoObjeto.java
@@ -33,22 +33,62 @@ public class GeracaoCodigoObjeto {
                     registradores.get(indexReg).setVariavel(lista.get(pos).getOperando1());
                     indexReg++;
                 }
-                
-                
                 if(lista.get(pos).getOperando2().charAt(0)=='(') {
                    int posEndereco = getPosEndereco(lista.get(pos).getOperando2());
                    Singleton.addComando(new ComandoMaquina("load",registradores.get(posRegistrador).getRegistrador(),"["+enderecos.get(posEndereco).getEndereco()+"]"));
                    
                 }
                 else {
-                    Singleton.addComando(new ComandoMaquina("load",registradores.get(posRegistrador).getRegistrador(),lista.get(pos).getOperando2()));
+                    if(lista.get(pos).getOperando2().charAt(0)>=48 && lista.get(pos).getOperando2().charAt(0)<=57){//se for um número
+                        Singleton.addComando(new ComandoMaquina("load",registradores.get(posRegistrador).getRegistrador(),lista.get(pos).getOperando2()));
+                    }
+                    else {
+                        int posRegistrador2 = getPosVariavel(lista.get(pos).getOperando2());
+                        Singleton.addComando(new ComandoMaquina("move",registradores.get(posRegistrador).getRegistrador(),registradores.get(posRegistrador2).getRegistrador()));
+                    }
+                    
                 }
             }
             if(SinalOperacao(lista.get(pos).getOperador())) {
+                String reg1 = "RF";
+                String reg2 = "RE";
+                //Analisar o operando1
+                if(lista.get(pos).getOperando1().charAt(0)=='(') { //se for (2)
+                   int posEndereco = getPosEndereco(lista.get(pos).getOperando1());
+                   Singleton.addComando(new ComandoMaquina("load","RF","["+enderecos.get(posEndereco).getEndereco()+"]"));
+                }
+                else {
+                    if(lista.get(pos).getOperando1().charAt(0)>=48 && lista.get(pos).getOperando1().charAt(0)<=57){ //se for um numero
+                        Singleton.addComando(new ComandoMaquina("load","RF",lista.get(pos).getOperando1()));
+                    }
+                    else {//se for uma variavel, procuro o registrador
+                        posRegistrador = getPosVariavel(lista.get(pos).getOperando1());
+                        reg1=registradores.get(posRegistrador).getRegistrador();
+                    }
+                }
+                //analisar o operando2
+                if(lista.get(pos).getOperando2().charAt(0)=='(') {//se for (2)
+                   int posEndereco = getPosEndereco(lista.get(pos).getOperando2());
+                   Singleton.addComando(new ComandoMaquina("load","RE","["+enderecos.get(posEndereco).getEndereco()+"]"));
+                }
+                else {
+                    if(lista.get(pos).getOperando2().charAt(0)>=48 && lista.get(pos).getOperando2().charAt(0)<=57){ //se for um numero
+                        Singleton.addComando(new ComandoMaquina("load","RE",lista.get(pos).getOperando2()));
+                    }
+                    else {//se for uma variavel, procuro o registrador
+                        posRegistrador = getPosVariavel(lista.get(pos).getOperando2());
+     
+                        reg2=registradores.get(posRegistrador).getRegistrador();
+                    }
+                }
+                
                 enderecos.add(new Endereco(""+enderecoAux,"("+lista.get(pos).getCodigo()+")"));
                 
-                Singleton.addComando(new ComandoMaquina(""));
+                Singleton.addComando(new ComandoMaquina("addi",reg1,reg2,"RD"));
+                Singleton.addComando(new ComandoMaquina("store","RD","["+enderecoAux+"]"));
+          
                 enderecoAux--;
+                
             }
             pos++;
         }
